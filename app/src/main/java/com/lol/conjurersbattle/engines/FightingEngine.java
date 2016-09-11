1304b77148574adaa46e045d7947ab3752505742
David Townsend <e393jz@erac.com>
2016-09-10 23:12:18 -0500

    creating mosnters

--- app/src/main/java/com/lol/conjurersbattle/engines/FightingEngine.java
+++ app/src/main/java/com/lol/conjurersbattle/engines/FightingEngine.java
@@ -3,17 +3,17 @@ package com.lol.conjurersbattle.engines;
 import com.lol.conjurersbattle.monster.Monster;
 
 public class FightingEngine {
-    Double maxMultiplier = 1.2;
-    Double minMultiplier = .8;
+    public static final Double maxMultiplier = 1.2;
+    public static final Double minMultiplier = .8;
 
-    Integer calculateAttackDamage(Monster attackMonster, Monster defendingMonster) {
+    public Integer calculateAttackDamage(Monster attackMonster, Monster defendingMonster) {
         Integer attackDamage = 0;
         Integer attack = attackRange(attackMonster.getAttack());
-        attackDamage = attack * (1000 / 3 * defendingMonster.getDefence());
+        attackDamage = attack * (1000 / (3 * defendingMonster.getDefence()));
         return attackDamage;
     }
 
-    Integer attackRange(Integer attack) {
+    private Integer attackRange(Integer attack) {
         Double range = maxMultiplier - minMultiplier + 1;
         return (int) (Math.random() * range) + attack;
     }
